fff83827804bb9c96fffe2094d5cb717f7777709
bloniea <bloniea@qq.com>
2023-03-25 16:56:38 +0800

    '完成'

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export default {
   owner: <string>'bloniea',
-  repo: <string>'images-bed',
+  repo: <string>'images-bed-18',
   // https://cdn.jsdelivr.net/gh/bloniea/images-bed/images1/1a9961425572ebe7.jpg
 
-  url: <string>'https://image-cdn.bloniea.ml/',
-  client_id: <string>'202a106a964267fa2de7',
+  url: <string>'https://image-cdn-18.bloniea.ml/',
+  client_id: <string>'ee0a15d4661829026097',
   scope: <string>'public_repo',
-  title: 'bloniea 姉さま的图床',
+  title: 'bloniea 姉さま的r18图床',
 }
