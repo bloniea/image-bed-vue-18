56fd8dcea7e6d1cedd91928983bf0c2c9e0fb266
bloniea <bloniea@qq.com>
2023-03-25 17:16:53 +0800

    '完成'

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export default {
   repo: <string>'images-bed-18',
   // https://cdn.jsdelivr.net/gh/bloniea/images-bed/images1/1a9961425572ebe7.jpg
 
-  url: <string>'https://image-cdn-18.bloniea.ml/',
+  url: <string>'https://image-cdn-18.bloniea.ml',
   client_id: <string>'ee0a15d4661829026097',
   scope: <string>'public_repo',
   title: 'bloniea 姉さま的r18图床',
