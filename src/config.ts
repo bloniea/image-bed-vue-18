836a93f3ce043aaadecba61024876a9ecf261e3c
bloniea <bloniea@qq.com>
2023-08-21 18:14:24 +0800

    更改域名

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export default {
   repo: <string>'images-bed-18',
   // https://cdn.jsdelivr.net/gh/bloniea/images-bed/images1/1a9961425572ebe7.jpg
 
-  url: <string>'https://image-cdn-18.bloniea.ml/',
+  url: <string>'https://images18.bloniea.com/',
   client_id: <string>'ee0a15d4661829026097',
   client_secret: <string>'5c77a74f9be87e8b941dfccb1a90180c0f6ff8c8',
   scope: <string>'public_repo',
